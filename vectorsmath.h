6d27c2d2f9bc7a49440da5ca19531e0302036679
Aditya Tomar <60942816+TomRigger@users.noreply.github.com>
2021-05-27 11:59:51 +0530

    Update vectorsmath.h

--- vectorsmath.h
+++ vectorsmath.h
@@ -1,4 +1,3 @@
-  
 #ifndef MATHS_H
 #define MATHS_H
 
@@ -10,13 +9,10 @@
 #define PI 3.1415926f
 #endif
 
-inline float sqr(float n)
-{
-	return n * n;
-}
+inline float sqr(float n) {
+	return n * n; }
 
-struct vector
-    {
+struct vector {
         float i, j, k; 
 
         vector();
@@ -43,50 +39,25 @@ struct vector
 float dot(vector v1, vector v2);
 vector cross(vector v1, vector v2);
 
-inline vector operator +(const vector& v1, const vector& v2)
-    {
- 	    return vector(v1.i + v2.i, v1.j + v2.j, v1.k + v2.k);
-    }
+inline vector operator +(const vector& v1, const vector& v2)	{	return vector(v1.i + v2.i, v1.j + v2.j, v1.k + v2.k);	}
 
-inline vector operator -(const vector& v1, const vector& v2)
-    {
-        return vector(v1.i - v2.i, v1.j - v2.j, v1.k - v2.k);
-    }
+inline vector operator -(const vector& v1, const vector& v2)	{	return vector(v1.i - v2.i, v1.j - v2.j, v1.k - v2.k);	}
 
-inline vector operator *(const vector& v1, const vector& v2)
-    {
-        return vector(v1.i * v2.i, v1.j * v2.j, v1.k * v2.k);
-    }
+inline vector operator *(const vector& v1, const vector& v2)	{	return vector(v1.i * v2.i, v1.j * v2.j, v1.k * v2.k);	}
 
-inline vector operator *(const vector& v, float f)
-    {
-        return vector(v.i * f, v.j * f, v.k * f);
-    }
+inline vector operator *(const vector& v, float f)	{	return vector(v.i * f, v.j * f, v.k * f);	}
 
-inline vector operator *(float f, const vector& v)
-    {
-        return vector(f * v.i, f * v.j, f * v.k);
-    }
+inline vector operator *(float f, const vector& v)	{	return vector(f * v.i, f * v.j, f * v.k);	}
 
-inline vector operator /(const vector& v1, const vector& v2)
-    {
-        return vector(v1.i / v2.i, v1.j / v2.j, v1.k / v2.k);
-    }
+inline vector operator /(const vector& v1, const vector& v2)	{	return vector(v1.i / v2.i, v1.j / v2.j, v1.k / v2.k);	}
 
-inline vector operator /(const vector& v, float f)
-    {
-        return vector(v.i / f, v.j / f, v.k / f);
-    }
+inline vector operator /(const vector& v, float f)	{	return vector(v.i / f, v.j / f, v.k / f);	}
 
-inline vector operator /(float f, const vector& v)
-    {
-        return vector(f / v.i, f / v.j, f / v.k);
-    }
+inline vector operator /(float f, const vector& v)	{	return vector(f / v.i, f / v.j, f / v.k);	}
 
 typedef vector Point;
 
-struct vector2
-    {
+struct vector2	{
         float u, v;
 
         vector2();
